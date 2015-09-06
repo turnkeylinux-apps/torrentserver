87aa2ee5f5165feb5309cb46bbf49280574b4557
Anton Pyrogovskyi <anton@turnkeylinux.org>
2015-09-06 18:28:29 +0000

    Update WebCP

--- overlay/var/www/index.php
+++ overlay/var/www/index.php
@@ -35,7 +35,7 @@
                     <div>
                         <a href="https://<?php print
                         $_SERVER{'HTTP_HOST'}; ?>:12322"><img
-                        src="images/download.png"/>MLDonkey</a>
+                        src="images/download.png"/>ruTorrent</a>
                     </div>
                     <div>
                         <a href="https://<?php print
@@ -49,60 +49,33 @@
                     </div>
                     <div>
                         <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>/files"><img
+                        $_SERVER{'HTTP_HOST'}; ?>/"><img
                         src="images/filemanager.png"/>File Manager</a>
                     </div>
+                    <div></div>
 
                     <h2>Resources and references</h2>
                     <ul>
                         <li>TurnKey Torrent Server <a href="http://www.turnkeylinux.org/torrentserver">release notes</a> and <a href="http://www.turnkeylinux.org/docs/torrentserver">documentation</a></li>
-                        <li>Wikipedia: <a href="http://en.wikipedia.org/wiki/BitTorrent_(protocol)">BitTorrent</a>, <a href="http://en.wikipedia.org/wiki/EDonkey_network">EDonkey network</a>, <a href="http://en.wikipedia.org/wiki/Kademlia">Kademlia</a></li>
-                        <li>MLDonkey <a href="http://mldonkey.sourceforge.net"> documentation</a></li>
+                        <li>Wikipedia: <a href="http://en.wikipedia.org/wiki/BitTorrent_(protocol)">BitTorrent</a> 
                     </ul>
-
                 </div>
             </div>
 
             <div id="tips">
                 <div class="fragment-content">
-                    <h2>Browser Extensions</h2>
-                    <div class="icon">
-                    <a href="http://www.turnkeylinux.org/docs/torrentserver/browserextensions/">
-                    <img src="/images/tsh64.png"/></a>
-                    </div>
-                    <p>Installing a compatible extension in your browser
-                    adds the capability to simply click on a download
-                    link (torrent, ed2k, magnet, sigdat) or
-                    <i>right-click -> Save with TorrentServer</i> any
-                    download link and have it automatically submitted to
-                    the Torrent Server for download.</p>
-                    <a href="http://www.turnkeylinux.org/docs/torrentserver/browserextensions/">Get them here</a>
-
                     <h2>Router/Firewall configuration</h2>
                     <p>TurnKey Torrent Server listens on certain ports, and
                     requires that they are reachable from the internet.
-                    As a quick reference the ports are specified below
-                    (perform a <a href="https://<?php print
-                    $_SERVER{'HTTP_HOST'}; ?>:12322/submit?q=porttest">
-                    port test</a>, and refer to the 
+                    As a quick reference the ports are specified below.
+                    Refer to the
                     <a href="http://www.turnkeylinux.org/docs/torrentserver">
-                    documentation</a> for more information).
+                    documentation</a> for more information.
                     </p>
                     <ul>
                         <li>BitTorrent: 6881-6889/TCP</li>
-                        <li>EDonkey: 2788/TCP, 2792/UDP</li>
-                        <li>Kademlia: 18247/UDP</li>
-                        <li>Overnet: 20883/TCP+UDP</li>
                         <li>Torrent Server Handler: 12322/TCP (optional)</li>
                     </ul>
-
-                    <h2>Email notifications</h2>
-                    <p>Configure your 
-                    <a href="https://<?php print
-                    $_SERVER{'HTTP_HOST'}; ?>:12322/submit?q=voo+6">
-                    email address</a> to receive notifications when downloads
-                    complete.</p>
-
                 </div>
             </div>
 
